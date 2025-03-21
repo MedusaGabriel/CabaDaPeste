a50c8f568d741220128fca23dcb2fd31470ce8b9
Gabriel Mutano <gabrieloliveirabarros1997@gmail.com>
2025-03-21 18:59:43 -0300

    🐛 fix: Bug NavMesh Removed(temp)

--- MY FIRST PROJECT/Assets/Enemy/Scripts/EnemyController.cs
+++ MY FIRST PROJECT/Assets/Enemy/Scripts/EnemyController.cs
@@ -1,18 +1,12 @@
 using UnityEngine;
-using UnityEngine.AI;  // Necessário para usar o NavMeshAgent
 
 public class EnemyAI : MonoBehaviour
 {
     public float speed = 3f; // Velocidade do inimigo
     private Transform player;
-    private NavMeshAgent agent;
 
     void Start()
     {
-        // Adicionando o NavMeshAgent ao inimigo
-        agent = GetComponent<NavMeshAgent>();
-        agent.speed = speed;  // Definindo a velocidade
-
         GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
         if (playerObj != null)
         {
@@ -31,8 +25,8 @@ public class EnemyAI : MonoBehaviour
     {
         if (player != null)
         {
-            // Definindo o destino do inimigo para a posição do player
-            agent.SetDestination(player.position);
+            Vector3 direction = (player.position - transform.position).normalized;
+            transform.position += direction * speed * Time.deltaTime;
         }
     }
 }
