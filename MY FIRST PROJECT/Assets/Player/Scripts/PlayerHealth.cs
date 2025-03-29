86fa8152d063d99ad86cb8dcb38467414116f6f9
Gabriel Mutano <gabrieloliveirabarros1997@gmail.com>
2025-03-29 00:53:06 -0300

    ❤️Feat: Invincibility

--- MY FIRST PROJECT/Assets/Player/Scripts/PlayerHealth.cs
+++ MY FIRST PROJECT/Assets/Player/Scripts/PlayerHealth.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     public int currentHealth;
     public GameObject gameOverCanvas;
-    public Button restartButton; 
+    public Button restartButton;
+
+    public bool isInvulnerable = false;
 
     void Start()
     {
@@ -19,6 +21,8 @@ public class PlayerHealth : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
+        if (isInvulnerable) return;
+
         currentHealth -= damage;
         if (currentHealth <= 0)
         {
@@ -26,18 +30,39 @@ public class PlayerHealth : MonoBehaviour
             Die();
         }
     }
+    public void ToggleInvincibility()
+    {
+        if (isInvulnerable)
+        {
+            DisableInvincibility();
+        }
+        else
+        {
+            EnableInvincibility();
+        }
+    }
+    public void EnableInvincibility()
+    {
+        Debug.Log("Invincibility Enabled!");
+        isInvulnerable = true;
+    }
 
+    public void DisableInvincibility()
+    {
+        Debug.Log("Invincibility Disabled!");
+        isInvulnerable = false;
+    }
     void Die()
     {
         Debug.Log("Player morreu!");
         gameOverCanvas.SetActive(true);
-        Time.timeScale = 0f; 
+        Time.timeScale = 0f;
     }
 
     public void RestartGame()
     {
-        Debug.Log("Botão Restart foi pressionado!"); 
-        Time.timeScale = 1f; 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
+        Debug.Log("Botão Restart foi pressionado!");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
