e3fe1ef7d82d4de8e4d6af2e20704e243d4b3061
Gabriel Mutano <gabrieloliveirabarros1997@gmail.com>
2025-04-16 20:10:04 -0300

    ☯️details; particles

--- MY FIRST PROJECT/Assets/Player/PlayerD/PlayerAttackMelee.cs
+++ MY FIRST PROJECT/Assets/Player/PlayerD/PlayerAttackMelee.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-// COMENTARIO TESTE PARA ENVIO MINHA PIROCA
+
 public class PlayerAttackMelee : MonoBehaviour
 {
     [Header("Referência para a Arma")]
@@ -8,6 +8,7 @@ public class PlayerAttackMelee : MonoBehaviour
     public float attackRange = 2f;
     public float attackDamage = 20f;
     public Animator playerAnimator;
+    public ParticleSystem weaponEffect; // Adicione esta linha
     public bool CanMove { get; private set; } = true;
     public bool IsAttacking { get; private set; } = false;
 
@@ -22,6 +23,11 @@ public class PlayerAttackMelee : MonoBehaviour
             {
                 _weaponCollider.enabled = false;
             }
+        }
+
+        if (weaponEffect != null)
+        {
+            weaponEffect.Stop(); // Certifique-se de que o efeito está desativado no início
         }
     }
 
@@ -38,6 +44,11 @@ public class PlayerAttackMelee : MonoBehaviour
         IsAttacking = true;
         playerAnimator.SetTrigger("PlayerMelee");
 
+        if (weaponEffect != null)
+        {
+            weaponEffect.Play(); // Ativa o efeito visual
+        }
+
         // Verifica todos os inimigos no alcance
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, attackRange);
         foreach (var hitCollider in hitColliders)
@@ -52,6 +63,7 @@ public class PlayerAttackMelee : MonoBehaviour
             }
         }
     }
+
     public void EnableCollider()
     {
         if (_weaponCollider != null)
@@ -71,6 +83,10 @@ public class PlayerAttackMelee : MonoBehaviour
         CanMove = true;
         playerAnimator.SetBool("IsAttacking", false);
         IsAttacking = false;
-    }
 
+        if (weaponEffect != null)
+        {
+            weaponEffect.Stop(); // Desativa o efeito visual
+        }
+    }
 }
