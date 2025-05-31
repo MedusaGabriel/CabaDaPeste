7c95d5425a3e7d80d176514ecda5147a66b3b749
Anderson Djalma <122568258+andersondjalma@users.noreply.github.com>
2025-05-31 19:08:11 -0300

    MenuConfigs

--- MY FIRST PROJECT/Assets/Canvas/Main Menu/Scripts/MainButtonScript.cs
+++ MY FIRST PROJECT/Assets/Canvas/Main Menu/Scripts/MainButtonScript.cs
@@ -9,6 +9,9 @@ public class MainButtonScript : MonoBehaviour, IPointerEnterHandler, IPointerExi
 
     private bool isHovered = false;
 
+    [Header("Som de Hover")]
+    public AudioClip hoverSound;
+
     void Start()
     {
         originalScale = transform.localScale;
@@ -23,10 +26,19 @@ public class MainButtonScript : MonoBehaviour, IPointerEnterHandler, IPointerExi
     public void OnPointerEnter(PointerEventData eventData)
     {
         isHovered = true;
+        TocarSomHover();
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         isHovered = false;
+    }
+
+    private void TocarSomHover()
+    {
+        if (hoverSound != null && AudioManager.Instance != null)
+        {
+            AudioManager.Instance.PlaySound(hoverSound);
+        }
     }
 }
--- MY FIRST PROJECT/Assets/Canvas/Main Menu/Scripts/MainMenu.cs
+++ MY FIRST PROJECT/Assets/Canvas/Main Menu/Scripts/MainMenu.cs
@@ -3,35 +3,55 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
     public GameObject mainMenuCanvas;
     public GameObject creditosCanvas;
     public GameObject opcoesCanvas;
 
-    // Chama quando clica em "Jogar"
+    [Header("Áudio")]
+    public AudioClip clickSound;
+
+    void Start()
+    {
+        // Não precisa mais de AudioSource aqui
+    }
+
+    private void TocarSomDeClique()
+    {
+        if (clickSound != null && AudioManager.Instance != null)
+            AudioManager.Instance.PlaySound(clickSound);
+    }
+
     public void AbrirJogo()
     {
+        TocarSomDeClique();
         SceneManager.LoadSceneAsync(1);
     }
+
     public void AbrirOpcoes()
     {
+        TocarSomDeClique();
         mainMenuCanvas.SetActive(false);
         opcoesCanvas.SetActive(true);
     }
+
     public void AbrirCreditos()
     {
+        TocarSomDeClique();
         mainMenuCanvas.SetActive(false);
         creditosCanvas.SetActive(true);
     }
+
     public void VoltarAoMenu()
     {
+        TocarSomDeClique();
         creditosCanvas.SetActive(false);
         opcoesCanvas.SetActive(false);
         mainMenuCanvas.SetActive(true);
     }
-    // Chama quando clica em "Sair"
-     public void SairDoJogo()
+
+    public void SairDoJogo()
     {
+        TocarSomDeClique();
         Debug.Log("Saindo do jogo...");
         Application.Quit();
     }
